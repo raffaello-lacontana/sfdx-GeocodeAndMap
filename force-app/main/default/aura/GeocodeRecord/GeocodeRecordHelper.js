a2bf2a5666d4afd8129e66f5b5f86234af7d9d2a
Raffaello La Contana <raffaello.lacontana@gmail.com>
2018-09-26 02:54:47 +0200

    New GeocodeRecord component

---
+++ force-app/main/default/aura/GeocodeRecord/GeocodeRecordHelper.js
@@ -0,0 +1,25 @@
+({ 
+    search : function(component, event, helper) { 
+    	var xmlHttp = new XMLHttpRequest();
+        var searchString = 'https://nominatim.openstreetmap.org/search/';
+    	searchString += cmp.find("searchString").get("v.value");
+        searchString += '?format=json&addressdetails=1&limit=1&polygon_svg=1';
+    	console.log(searchString);
+    	xmlHttp.open( "GET", searchString, true );
+	    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+		xmlHttp.responseType = 'text';
+		xmlHttp.onload = function () {
+    	    console.log("onload");
+        	console.log(xmlHttp.readyState);
+        	console.log(xmlHttp.status);
+    		if (xmlHttp.readyState === 4) {
+    	    	if (xmlHttp.status === 200) {
+	            	console.log(xmlHttp.response);
+            		console.log(xmlHttp.responseText);
+		        }
+	    	}
+		};
+	    xmlHttp.send( null );
+	    console.log("Request sent");
+    }
+})
